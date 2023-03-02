04e8bbcf40df9632d3a0b0a43f8e7cba7dc02803
tylerjmwhit <89553397+tylerjmwhit@users.noreply.github.com>
2023-03-01 17:24:26 -0800

    Added in BFS logic. Just need to add in logic to ouput the actual string and user input

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
+from queue import Queue
+
+def BFS(N:int, D:set):
+    #Making sure D is in ascending order
+    D = sorted(D)
+    #initalizing our arrays and queues
+    q = Queue(maxsize = 0)
+    parent = [-1]*N
+    label = [-1]*7
+    visited = [False]*N
+    for j in D:
+        next = delta(0,j,N)
+        visited[next] = True
+        q.put(next)
+        parent[next] = 0
+        label[next] = j
+    while not q.empty():
+        if next == 0:
+            break
+        cur = q.get_nowait()
+        for i in D:
+            next = delta(cur, i, N)
+            if next == 0:
+                parent[next] = cur
+                label[next] = i
+                break
+            elif not visited[next]:
+                visited[next] = True
+                parent[next] = cur
+                label[next] = i
+                q.put(next)
+    if next != 0:
+       return print("no soultion")
+    else:
+        return
+        # trace the string using parent pointers and
+        #  concatenate the corresponding labels as you trace until
+        #  start state is reached output the reverse of the string.
+
+
+
 
 
 
 def delta(cur: int, Input: int, N:int):
     return (10*cur + Input) % N
 
-    
+
 def main():
-    delta()
+    BFS(7,{3,1})
 
 if __name__ == '__main__':
     main()
