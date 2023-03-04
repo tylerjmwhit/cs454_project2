e32e29dde9077bd3082ced97fe8e3438bb8c243c
Jeremy Pearson <pearsonje@sonoma.edu>
2023-03-04 14:42:49 -0800

    Added user input and output for correct answer. Fixed bug with array and queue initialization

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def BFS(N:int, D:set):
     #initalizing our arrays and queues
     q = Queue(maxsize = 0)
     parent = [-1]*N
-    label = [-1]*7
+    label = [-1]*N
     visited = [False]*N
     # loop for setting up arrays and queues with starting state of zero
     for j in D:
         next = delta(0,j,N)
         visited[next] = True
-        q.put(next)
+        if j != 0:
+            q.put(next)
         parent[next] = 0
         label[next] = j
     # performing the BFS
@@ -34,10 +35,17 @@ def BFS(N:int, D:set):
     if next != 0:
        return print("no soultion")
     else:
-        return
         # trace the string using parent pointers and
         #  concatenate the corresponding labels as you trace until
         #  start state is reached output the reverse of the string.
+        i = 0
+        output = ''
+        while (parent[i] != 0):
+            output += str(label[i])
+            i = parent[i]
+        output += str(label[i])
+        return print(output[: : -1])
+        
 
 
 
@@ -49,9 +57,27 @@ def delta(cur: int, Input: int, N:int):
 
 
 def main():
-    BFS(7,{3,1})
+    val = 0
+    while val != -1:
+        val = int(input("Enter a positive integer N, to compute for, Enter -1 to exit: "))
+        if val == -1:
+            break
+        #Create a list of strings split by white space to be converted to integers
+        stringDigits = input("Enter a list of allowed digits seperated by a space. Digits must be an integer from 0 to 9: ").split()
+        intDigits = [eval(i) for i in stringDigits]
+        if all(i >= 9 and i <= 0  for i in intDigits):
+            print("List of digits must contain integers between 0 and 9")
+        elif val > 0:
+            BFS(val , intDigits)
+        else:
+            print("Please enter a number between Greater than 0 ")
 
 if __name__ == '__main__':
     main()
 
 
+11000001001000011
+11000001001000011
+
+11011011011001001
+11011011011001001
